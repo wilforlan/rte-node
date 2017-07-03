3f19b8d4093f0d3c077f6ab52e2e21fb31084e6d
McZlatan Bourne <bournethesonofthegun@gmail.com>
2017-07-03 13:46:13 +0100

    Added new endponts

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
 var RTE = require('./index.js');
 
-var rte = new RTE('T5950','a5ca36b2a932ad8759f1b1930f495eed','$2a$10$.Mr2xpTDz5FrPp8sQxVsPud3KTJJvMCEm3tWmnVbWfSPEUnIrkwRe', 'http://localhost:3000');
+//var rte = new RTE('T5950','a5ca36b2a932ad8759f1b1930f495eed','$2a$10$.Mr2xpTDz5FrPp8sQxVsPud3KTJJvMCEm3tWmnVbWfSPEUnIrkwRe', 'http://localhost:3000');
+var rte = new RTE('T541','df06f76cc0ee9212dfe9a6ef84a5ed04','$2a$10$31zmBTMWR92ixIiRLIaDce02fZNDqC30yPEbn50b7KOUI6z2rRQEW', 'http://localhost:3000');
 
 
 // rte.Bank.getAllBanks(function(error, body){
@@ -13,27 +14,45 @@ var rte = new RTE('T5950','a5ca36b2a932ad8759f1b1930f495eed','$2a$10$.Mr2xpTDz5F
 
 
 
-// var user = {
-// 	user_id: '8383748',
-// 	username : "",
-// 	email: 'james@mail.com',
-// 	first_name: 'James',
-// 	last_name: "Nikon",
-// 	phone_number: '234814677368',
-// 	bvn: '22133388729',
-// }
+ //var user = {
+ //	user_id: '8383748',
+ //	username : "",
+ //	email: 'james@mail.com',
+ //	first_name: 'James',
+ //	last_name: "Nikon",
+ //	phone_number: '234814677368',
+ //	bvn: '22133388729',
+ //}
+ //
+ //
+ //rte.User.create(user, function(error, body){
+ //	console.log(body);
+ //});
 
-
-// rte.User.create(user, function(error, body){
-// 	console.log(body);
-// });
-
+ // var options = {
+ //	skip : 0,
+ //	limit : 20,
+ //	user_id : "8383748"
+ //};
+ //
+ //rte.User.find(options, function(error, body){
+ //	//console.log(body);
+ //   var user = body.payload.users[0];
+ //   console.log(user);
+ //   
+ //});
+ 
 // var options = {
-// 	skip : 0,
-// 	limit : 20,
+//	"user_id": "8383748",
+//    "card_type": "mastercard",
+//    "number": "111122233334444",
+//    "issuer": "STANBIC",
+//    "cvv2": "123",
+//    "expiry_month": "07",
+//    "expiry_year": "17"
 // };
-
-// rte.User.find(options, function(error, body){
+//
+// rte.User.updateUserCard(options, function(error, body){
 // 	console.log(body);
 // });
 
